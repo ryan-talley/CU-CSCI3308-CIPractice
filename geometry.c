04c1343d6ca1281f442990277fa627f26e1ea73d
user <ryta9312@colorado.edu>
2015-12-01 19:49:39 -0700

    shit

--- geometry.c
+++ geometry.c
@@ -19,6 +19,13 @@
 
 #define DEBUG(file, line, func, msg) fprintf(stderr, "DEBUG - %s_%d_%s: %s", file, line, func, msg);
 
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c)
+{
+  double area = 0.0;
+  area = abs(((a->x * (b->y - c->y)) + (b->x * (c->y - a->y)) + (c->x * (a->y - b->y))) / 2.0);
+  return area;
+}
+
 double coord_2d_dist(const coord_2d_t* a, const coord_2d_t* b){
 
     /* Input Checks */
